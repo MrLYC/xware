50ad7ec7aa6802dbad318247f1066b5e10866424
刘奕聪 <imyikong@gmail.com>
2016-10-30 10:29:20 +0800

    tail xware_portal.log

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 /run.sh &
+touch xware_portal.log
+tail -f xware_portal.log &
 ./portal > xware_portal.log
 pid=
 trap '[[ $pid ]] && kill $pid; ./portal -s; exit 0' SIGINT SIGTERM
 while :
 do
 	sleep 60 & pid=$!
-	wait
-	pid=
+	waitd	pid=
 
 	# 监控重启
         ps -fe | grep ETMDaemon | grep -v grep
